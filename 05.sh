8cba2ec4a889d9c71b6a70b819eb66ab78420d23
manikantareddym <mmkreddi1984@gmail.com>
2022-01-13 16:08:59 +0530

    add

---
+++ 05.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+out() {
+
+    if [$? -eq 0] ; then
+        echo "Success"
+    else
+        echo "Failure"
+}
+
+Log = /tmp/logs
+
+echo "print $(df -h)" &>>$Log
+out $?
+echo "print $(df -dffgh)" &>>$Log
+out $?
+echo "print $(df -h)" &>>$Log
+out $?
