0e4d7798a25f4c23a97b96e7d119313a634283c4
manikantareddym <mmkreddi1984@gmail.com>
2022-01-13 17:08:46 +0530

    add

--- project.sh
+++ project.sh
@@ -12,6 +12,6 @@ Log=/tmp/out1.log
 
 yum install httpd -y >&Log
 out $?
-echo 'ProxyPass "/student" "http://APP-SERVER-IPADDRESS:8080/student"
-ProxyPassReverse "/student"  "http://APP-SERVER-IPADDRESS:8080/student"' > /etc/httpd/conf.d/proxy.conf >&Log
+echo "ProxyPass "/student" "http://APP-SERVER-IPADDRESS:8080/student"
+ProxyPassReverse "/student"  "http://APP-SERVER-IPADDRESS:8080/student" " > /etc/httpd/conf.d/proxy.conf >&Log
 out $?
